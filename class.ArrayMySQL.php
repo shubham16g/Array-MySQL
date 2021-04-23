bb7bd12c299ef5403d4eb52ac5b95f5460993869
Shubham Gupta <shubham.developer16@gmail.com>
2021-04-23 18:49:12 +0530

    Implemented MySQL Prepare

--- class.ArrayMySQL.php
+++ class.ArrayMySQL.php
@@ -1,15 +1,22 @@
 <?php
-class ArrayMySQL {
-	
-	// private $conn;
-	private const COLON = "'";
-	
-	public function __construct(mysqli $db) {
+class ArrayMySQL
+{
+	public const ERROR_MYSQLI_QUERY_MSG = 'Error in mysqli query';
+	public const ERROR_MYSQLI_QUERY_CODE = 964;
+	public const ERROR_MYSQLI_CONNECT_MSG = 'Error in mysqli connection';
+	public const ERROR_MYSQLI_CONNECT_CODE = 458;
+
+	public function __construct(mysqli $db)
+	{
+		if ($db->connect_errno) {
+			throw new Exception(self::ERROR_MYSQLI_CONNECT_MSG, self::ERROR_MYSQLI_CONNECT_CODE);
+		}
 		$db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, TRUE);
 		$this->db = $db;
 	}
 
-	public function isConnected(){
+	public function isConnected()
+	{
 		if ($this->db->isConnected()) {
 			return true;
 		} else {
@@ -17,100 +24,115 @@ class ArrayMySQL {
 		}
 	}
 
-	public function deleteSQL($table, $extras) {
-		$query = `DELETE FROM $table $extras`;
-		return $this->staticSQL($query);
+	public function insertSQL(string $table, array $valueArr)
+	{
+		$columns = "";
+		$values = "";
+		$array = [];
+		foreach ($valueArr as $key => $value) {
+			// $key = $this->escapeColonSQL($key);
+			$columns .= "$key, ";
+			$values .= ",? ";
+			$array[] = $value;
+		}
+		$columns = substr($columns, 0, -2);
+		$values = substr($values, 0, -2);
+		$query = "INSERT INTO $table ($columns) VALUES ($values)";
+		$this->staticSQL($query, $array);
 	}
 
-	public function updateSQL($table, $newArr, $extras)	{
+	public function deleteSQL(string $table, string $extras, array $array = null)
+	{
+		$query = `DELETE FROM $table $extras`;
+		$this->staticSQL($query, $array);
+	}
+
+	public function updateSQL(string $table, array $newArr, string $extras, array $array = null)
+	{
 		$str = "";
 		foreach ($newArr as $key => $value) {
-			$value = $this->escapeColonSQL($value);
+			if (gettype($value) == 'string')
+				$value = "'" . $this->real_escape($value) . "'";
+			else
+				$value = $this->real_escape($value);
 			$str = "$str $key = $value,";
 		}
 		$str = substr($str, 0, -1);
-
 		$query = "UPDATE $table SET $str $extras";
-		return $this->staticSQL($query);
+		$this->staticSQL($query, $array);
 	}
 
-	public function insertSQL($table, $valueArr) {
-		$columns = "";
-		$values = "";
-		foreach ($valueArr as $key => $value) {
-			// $key = $this->escapeColonSQL($key);
-			$value = $this->escapeColonSQL($value);
-			$columns = "$columns$key, ";
-			$values = "$values$value, ";
-		}
-		$columns = substr($columns, 0,-2);
-		$values = substr($values, 0,-2);
-		$query = "INSERT INTO $table ($columns) VALUES ($values)";
-		return $this->staticSQL($query);
-		
+	public function selectSQL(string $table, string $columns, string $extras, array $array = null)
+	{
+		return $this->arraySQL("SELECT $columns FROM $table $extras", $array);
 	}
 
-	public function selectSQL($table, $columns, $extras){
-		return $this->arraySQL("SELECT $columns FROM $table $extras");
+	public function selectPageSQL(int $itemPerPage = 20, int $page = 1, string $table, string $columns, string $extras, array $array = null)
+	{
+		return $this->arrayPageSQL($itemPerPage, $page, "SELECT $columns FROM $table $extras", $array);
 	}
 
-	public function selectPageSQL($table, $columns, $extras, $itemPerPage, $page){
+	public function countSQL(string $table, string $extras, array $array)
+	{
+		$result_pages = $this->arraySQL("SELECT count(*) FROM $table $extras", $array);
+		return (int) $result_pages[0];
+	}
+
+	public function getPagesCount(string $count, string $itemPerPage = 20)
+	{
+		$total_pages = ceil($count / $itemPerPage);
+		return (int) $total_pages;
+	}
+
+	public function staticSQL(string $baseQuery, array $array = null)
+	{
+		$stmt = $this->_prepare_stmt($baseQuery, $array);
+		$stmt->execute();
+	}
+
+	public function arrayPageSQL(int $itemPerPage = 20, int $page = 1, string $baseQuery, array $array = null)
+	{
 		$offset = ($page - 1) * $itemPerPage;
-		return $this->arraySQL("SELECT $columns FROM $table $extras LIMIT $offset, $itemPerPage");
+		$baseQuery .= " LIMIT $offset, $itemPerPage";
+		return $this->arraySQL($baseQuery, $array);
 	}
 
-	public function getPagesCount($table, $extras, $itemPerPage){
-        $total_rows = $this->countSQL($table, $extras);
-        $total_pages = ceil($total_rows / $itemPerPage);
-        return (int) $total_pages;
-    }
-
-    public function countSQL($table, $extras) {
-    	$result_pages = $this->db->query("SELECT count(*) FROM $table $extras");
-    	return (int) $result_pages->fetch_array()[0];
-    }
-
-	public function staticSQL($query){
-		if ($this->runSQL($query)) {
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
-	public function arraySQL($query){
-		$res = $this->runSQL($query);
-		if (!$res) {
-			return null;
-		}
+	public function arraySQL(string $baseQuery, array $array = null)
+	{
+		$stmt = $this->_prepare_stmt($baseQuery, $array);
+		$stmt->execute();
+		$res = $stmt->get_result();
 		$arr = array();
 		while ($row = $res->fetch_assoc()) {
 			$arr[] = $row;
 		}
-
-		// todo
-		// mysqli_free_result($res);
+		$res->free_result();
 		return $arr;
 	}
 
-	public function escapeSQL($value){
-		return $this->db->real_escape_string($value);	
-	}
-
-	private function escapeColonSQL($value) {
-		$RR = self::COLON;
-		return "$RR" . $this->escapeSQL($value) . "$RR";
-	}
-
-	private function runSQL($query){
-		$res = $this->db->query($query);
-		return $res;
-	}
-
-	public function close()
+	private function _prepare_stmt(string $baseQuery, array $array = null)
 	{
-		$this->db->close();
+		$count = substr_count($baseQuery, "?");
+		if ($count != sizeof($array)) {
+			throw new Exception(self::ERROR_MYSQLI_QUERY_MSG, self::ERROR_MYSQLI_QUERY_CODE);
+		}
+		$ps = '';
+		foreach ($array as $key => $value) {
+			$ps .= 's';
+			$array[$key] = $this->real_escape($value);
+		}
+
+		$stmt = $this->db->prepare($baseQuery);
+		if (!$stmt) {
+			throw new Exception(self::ERROR_MYSQLI_QUERY_MSG, self::ERROR_MYSQLI_QUERY_CODE);
+		}
+		$stmt->bind_param($ps, ...$array);
+		return $stmt;
 	}
-}	
-?>
+
+	// todo
+	public function real_escape($value)
+	{
+		return $this->db->real_escape_string($value);
+	}
+}
